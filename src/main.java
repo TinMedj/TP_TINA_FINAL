1cbcaa7489c7aa03cb116905672bb632fb8dc267
TinMedj <ft_medjkoune@esi.dz>
2017-10-17 21:45:37 +0100

    commenter le main

--- src/main.java
+++ src/main.java
@@ -8,25 +8,23 @@ public class main {
 
 
         Vector<Integer> vec1=new Vector<Integer>();
-
         Vector<Integer> vec2=new Vector<Integer>();
         Vector<Integer> vec3=new Vector<Integer>();
         Vector<Integer> min_max=new Vector<Integer>();
-        System.out.print("initialisation du vecteur01");
-        //teste conflit
+        System.out.print("initialisation du vecteur01");//initialisation du vecteur 01
         VectorHelper.creer_vector(vec1,5);
         System.out.print("\n affichage du contenu du vecteur01 ");
         VectorHelper.afficher_vector(vec1);
         VectorHelper.inverser_vector(vec1);
-        System.out.print("\n inversion du vecteur 01 :");
+        System.out.print("\n inversion du vecteur 01 :"); //inverser le vecteur 01
         VectorHelper.afficher_vector(vec1);
-        System.out.print("\n Trier le vecteur01 ");
+        System.out.print("\n Trier le vecteur01 ");  //trier le vecteur 01
         VectorHelper.trier_vector(vec1);
         VectorHelper.afficher_vector(vec1);
-        System.out.print("\n min et max du vecteur01: ");
+        System.out.print("\n min et max du vecteur01: "); //trouver le min et max du vecteur
         VectorHelper.min_max(vec1,min_max);
         System.out.print("\n le min est : "+min_max.elementAt(0)+" \n le max est : "+min_max.elementAt(1));
-        System.out.print("\n application de la formule pour le vecteur01:  ");
+        System.out.print("\n application de la formule pour le vecteur01:  "); //applicaion de la formuler 01
         VectorHelper.formule(vec1);
         VectorHelper.afficher_vector(vec1);
         System.out.print("initialisation du vecteur02");
